8f3ad217df83b6f114d137ef95ce08065ccbbf72
Syah Muhamad Rizky <syahmuhamadrizky@gmail.com>
2026-06-21 23:57:52 +0700

    Release v1.2.1 - Fix sync arsip and dashboard stats

--- dist/assets/refresh-ccw-xSVZH4gh.js
+++ dist/assets/refresh-ccw-xSVZH4gh.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CQRt2Rww.js";/**
+import{c as e}from"./index-QwSoRxOX.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/trash-2-CYFyoc-J.js
+++ dist/assets/trash-2-CYFyoc-J.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CQRt2Rww.js";/**
+import{c as a}from"./index-QwSoRxOX.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
