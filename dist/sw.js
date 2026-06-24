d1a4ffffabee5d6980613ce8a2c1772a4a39b39a
Syah Muhamad Rizky <syahmuhamadrizky@gmail.com>
2026-06-25 00:27:00 +0700

    Release v1.3.91

---
+++ dist/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let the browser do its default thing
+  // for non-GET requests.
+});
